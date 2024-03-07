d8001ea73c4dec0f2f91bb4e7fafacf0b26c877e
AlexandreBasei <alexandreacevedobasei@gmail.com>
2024-03-07 17:50:12 +0100

    morpion

--- app.js
+++ app.js
@@ -4,23 +4,25 @@ const express = require('express');
 
 const app = express();
 const http = require('http').createServer(app);
-const path =require('path');
+const path = require('path');
 const port = 8080;
 
 /**
  * @type {Socket}
  */
-
 const io = require('socket.io')(http);
 
 app.use('/bootstrap/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/bootstrap/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'templates/index.html'));
+});
+
+app.get('/games/tic-tac-toe', (req, res) => {
+    res.sendFile(path.join(__dirname, 'templates/games/tic-tac-toe.html'));
 });
 
 http.listen(port, () => {
@@ -34,22 +36,25 @@ io.on('connection', (socket) => {
 
     socket.on('playerData', (player) => {
         console.log(`[playerData] ${player.username}`);
+
         let room = null;
-        
+
         if (!player.roomId) {
             room = createRoom(player);
-            console.log(`[create room] - ${room.id} - ${player.username}`);
-        } else{
+            console.log(`[create room ] - ${room.id} - ${player.username}`);
+        } else {
             room = rooms.find(r => r.id === player.roomId);
 
             if (room === undefined) {
                 return;
             }
 
+            player.roomId = room.id;
             room.players.push(player);
         }
 
         socket.join(room.id);
+
         io.to(socket.id).emit('join room', room.id);
 
         if (room.players.length === 2) {
@@ -62,11 +67,20 @@ io.on('connection', (socket) => {
     });
 
     socket.on('play', (player) => {
-        io.to(socket.id).emit('play', player);
+        console.log(`[play] ${player.username}`);
+        io.to(player.roomId).emit('play', player);
+    });
+
+    socket.on('play again', (roomId) => {
+        const room = rooms.find(r => r.id === roomId);
+
+        if (room && room.players.length === 2) {
+            io.to(room.id).emit('play again', room.players);
+        }
     })
 
     socket.on('disconnect', () => {
-        console.log(`[disconnect] - ${socket.id} - `);
+        console.log(`[disconnect] ${socket.id}`);
         let room = null;
 
         rooms.forEach(r => {
@@ -77,19 +91,20 @@ io.on('connection', (socket) => {
                 }
             })
         })
-    })
+    });
 });
 
-const createRoom = (player) => {
-    const room = {id: roomId(), players: []};
+function createRoom(player) {
+    const room = { id: roomId(), players: [] };
 
     player.roomId = room.id;
+
     room.players.push(player);
-    rooms.push(room)
+    rooms.push(room);
 
     return room;
 }
 
-const roomId = () => {
-    return Math.random().toString(36).substring(2, 9);
+function roomId() {
+    return Math.random().toString(36).substr(2, 9);
 }
